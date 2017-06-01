0338bfd40f9374dcde3513f52c48660030b83909
Ammonite <ammonite.myfox@gmail.com>
2017-06-01 23:35:50 +0200

    Install script

--- install/main.sh
+++ install/main.sh
@@ -2,14 +2,41 @@
 set -e
 set -x
 
+if [[ $EUID -ne 0 ]]
+then
+   echo "Please run as root"
+   exit 1
+fi
+
 apt-get update
-apt-get upgrade -y
 
-apt-get install htop git curl rsync
-apt-get install python-dev supervisor
+apt-get install -y htop git curl rsync
+apt-get install -y python-dev supervisor
 
-curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-python get-pip.py
+pip_test=0
+command -v pip || pip_test=1
+
+if [[ $pip_test -ne 0 ]]
+then
+    curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+    python get-pip.py
+fi
 
 pip install -U pip
-pip install unicornhat
+pip install unicornclient
+
+supervisor_configuration="[program:unicornclient]
+user=root
+directory=/root
+command=unicornclient
+autostart=true
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/dev/shm/unicornclient.log
+stopsignal=KILL
+stopasgroup=true"
+
+supervisor_path="/etc/supervisor/conf.d/"
+if [ -d "$supervisor_path" ]; then
+    echo "$supervisor_configuration" > $supervisor_path/unicornclient.conf
+fi
