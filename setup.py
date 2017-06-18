1e0dfa3b3b80977f7f13c740874873fb234140b2
Ammonite <ammonite.myfox@gmail.com>
2017-06-18 23:16:56 +0200

    v3.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CLASSIFIERS = ['Development Status :: 3 - Alpha',
 
 setup(
     name             = 'unicornclient',
-    version          = '2.4.2',
+    version          = '3.0.0',
     author           = 'Pierre Cavan',
     author_email     = 'ammonite.myfox@gmail.com',
     description      = """Unicorn client""",
