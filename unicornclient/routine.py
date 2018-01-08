8dc1bab80e52442999eb59e096abd5848c4e8d66
Ammonite <ammonite.myfox@gmail.com>
2018-01-08 22:23:18 +0100

    Allow one last call to process before stopping

--- unicornclient/routine.py
+++ unicornclient/routine.py
@@ -7,6 +7,7 @@ class Routine(threading.Thread):
         self.queue = queue.Queue()
         self.manager = None
         self.no_wait = False
+        self.is_stopping = False
 
     def run(self):
         while True:
@@ -29,12 +30,15 @@ class Routine(threading.Thread):
                 routine_command = data[index] if index in data else None
 
                 if routine_command == 'stop':
-                    return
+                    self.is_stopping = True
 
             self.process(data)
 
             if got_task:
                 self.queue.task_done()
+
+            if self.is_stopping:
+                break
 
     def process(self, data):
         pass
