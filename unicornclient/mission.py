2b88683ce1a54fb02bfc13bd05b266a0528ce145
Ammonite <ammonite.myfox@gmail.com>
2018-02-24 23:24:32 +0100

    Add post method

--- unicornclient/mission.py
+++ unicornclient/mission.py
@@ -1,7 +1,19 @@
 
+import json
+
+from . import message
+
 class Mission(object):
     def __init__(self, sender):
         self.sender = sender
 
     def send(self, message):
         self.sender.send(message)
+
+    def post(self, name, data):
+        msg = message.Message({ 'type': 'mission', 'name': name })
+        if isinstance(data, dict):
+            msg.set_body(json.dumps(data).encode())
+        else:
+            msg.set_body(data)
+        self.sender.send(msg)
